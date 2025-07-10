9bb2a67810a86da0b4ec7c3a8e5f17355e52e93a
Harisaran <harisaran777s@gmail.com>
2025-07-11 03:56:00 +0530

    Manual conversions

--- ManualConversion.java
+++ ManualConversion.java
@@ -5,9 +5,11 @@ public class ManualConversion
 		double d = 100.04;
 		long l = (long)d;
 		int i = (int)l;
+		char c = (char)i;
 		System.out.println("Double :"+d);
 		System.out.println("Long :"+l);
 		System.out.println("Int :"+i);
+		System.out.println("Char :"+c);
 	}
 }
 
