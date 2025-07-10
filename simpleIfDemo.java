f44c13d3ee5461d4c556f198be131380a0161bf8
Harisaran <harisaran777s@gmail.com>
2025-07-10 21:16:55 +0530

    Simple odd or even program

--- simpleIfDemo.java
+++ simpleIfDemo.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class simpleIfDemo
 {
-        public static void main (String[] args)
+    public static void main (String[] args)
 	{
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter an integer :");
 		int number = input.nextInt();
-		if (number % 5 == 0 )
+		if (number % 2 == 0 )
 			System.out.println(number + " is even");
 		else 
 			System.out.println(number + " is odd");
