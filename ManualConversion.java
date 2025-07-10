c1e6948e076ddb0db4069d5637094c2c77bf7948
Harisaran <harisaran777s@gmail.com>
2025-07-11 03:54:03 +0530

    Manual conversion

---
+++ ManualConversion.java
@@ -0,0 +1,13 @@
+public class ManualConversion
+{
+	public static void main(String[] args)
+	{
+		double d = 100.04;
+		long l = (long)d;
+		int i = (int)l;
+		System.out.println("Double :"+d);
+		System.out.println("Long :"+l);
+		System.out.println("Int :"+i);
+	}
+}
+
