296d5ec9cd8a94002c0db98bd822194c21e78037
Harisaran <harisaran777s@gmail.com>
2025-07-11 03:50:44 +0530

    Just auto conversion

---
+++ Conversion.java
@@ -0,0 +1,14 @@
+public class Conversion 
+{
+	public static void main (String[] args)
+	{
+		char c='J';
+		int i =c;
+		long l=i;
+		float f=l;
+		System.out.println("Int :"+i);
+		System.out.println("Char :"+c);
+		System.out.println("long :"+l);
+		System.out.println("float :"+f);
+	}
+}
