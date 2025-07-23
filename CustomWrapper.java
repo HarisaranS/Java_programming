8eaa87b29ad6e83122aa68378517d86f9af6048d
Harisaran <harisaran777s@gmail.com>
2025-07-23 23:36:17 +0530

    Using Custom Wrapper class

---
+++ CustomWrapper.java
@@ -0,0 +1,27 @@
+class speedWrapperClass{
+
+	private int speed;
+
+ 	//speedWrapperClass(){}
+
+	speedWrapperClass(int speed){
+		this.speed = speed;
+	}
+
+	public int getVehicleSpeed(){
+		return speed;
+	}
+
+	public void setVehicleSpeed(int speed){
+		this.speed = speed;
+	}
+}
+public class CustomWrapper{
+	public static void main (String[] args){
+		speedWrapperClass speedValue = new speedWrapperClass(100);
+		System.out.println("Vehile Speed : "+speedValue.getVehicleSpeed() + "km/hr");
+	}
+}
+
+
+
