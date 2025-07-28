b1dcf3cf029a23bf72b19040ecd1a7a8654eb9e9
Harisaran <harisaran777s@gmail.com>
2025-07-28 23:35:06 +0530

    simple Array object creation

---
+++ ArrayObject.java
@@ -0,0 +1,16 @@
+class Student {
+	Student(int id, String name){
+		System.out.println("Student ID is "+id+" and name is "+name);
+	}
+}
+
+public class ArrayObject {
+	public static void main (String[] args){
+		//declaring an array of object 
+		Student obj[] = new Student[3];
+
+		obj[0] = new Student(1, "Bharat");
+		obj[1] = new Student(5,"Vivaan");
+		obj[2] = new Student(6,"Smith");
+	}
+}
