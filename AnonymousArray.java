83110319246f2d39d6dccef478d5f7fa13833ccc
Harisaran <harisaran777s@gmail.com>
2025-07-31 04:24:50 +0530

    simple Anonymous Array

---
+++ AnonymousArray.java
@@ -0,0 +1,17 @@
+class AnonymousArray{
+	public static void main (String[] args){
+
+		//Anonymous array
+		sum(new int[]{1,2,3});
+	}
+
+	public static void sum(int[] a ){
+		int total =0;
+
+		//using for each loop 
+		for (int i: a)
+			total = total + i;
+
+		System.out.println("The sum is: "+total);
+	}
+}
