09dbc4137e7eec1f638266563cb918be52ec417f
Harisaran <harisaran777s@gmail.com>
2025-08-01 10:44:32 +0530

    Cloning an Array

---
+++ CloneArray.java
@@ -0,0 +1,17 @@
+public class CloneArray{
+	public static void main(String[] args){
+		//decalring and initializing an array 
+		//Jagged array
+		int myArray[][] = {{1,2,3} ,{4,5,6}};
+
+		//clone myArray by using clone property
+		int cloneArray[][] = myArray.clone();
+
+		//checking whether both arrays are the same or not 
+		System.out.println(myArray == cloneArray);
+
+		//checking whether both arrays have the same elements
+		System.out.println(myArray[1] == cloneArray[1]);
+	}
+}
+
