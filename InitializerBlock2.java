0a0cd7d4337f05a3cb1b82c9eb5632bef92199c1
Harisaran <harisaran777s@gmail.com>
2025-08-08 15:13:54 +0530

    Simple Initializerblock

---
+++ InitializerBlock2.java
@@ -0,0 +1,18 @@
+class Bike{
+	int speed;
+	//Constructor
+	Bike(){
+		System.out.println("speed is "+speed);
+	}
+	//Instance Initializer block
+	{
+		speed = 100;
+	}
+}
+
+public class InitializerBlock2{
+	public static void main(String[] args){
+		Bike b1 = new Bike();
+		Bike b2 = new Bike();
+	}
+}
