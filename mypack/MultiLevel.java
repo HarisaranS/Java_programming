40d89b110c5c65cc094dbd8b0e4b24ad187bf30d
Harisaran <harisaran777s@gmail.com>
2025-09-14 10:26:30 +0530

    Multilevel using package

---
+++ mypack/MultiLevel.java
@@ -0,0 +1,14 @@
+package mypack;
+import mypack.*;
+
+public class MultiLevel{
+	public static void main(String[] args){
+		ClassC c = new ClassC();
+
+		c.dispA();
+		c.dispB();
+		c.dispC();
+	}
+}
+
+
