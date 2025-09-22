b94a91da1ee602183d3f3e3e3d3aae3a8eea628e
Harisaran <harisaran777s@gmail.com>
2025-09-22 23:32:36 +0530

    Using CharAt function

---
+++ CharAtEg.java
@@ -0,0 +1,11 @@
+public class CharAtEg{
+	public static void main(String[] args){
+		String str = "Welcome to JavatPoint portal";
+		for (int i=0; i<=str.length()-1 ; i++){
+			if (i%2!=0){
+				System.out.println("Char at "+i+" place "+str.charAt(i));
+			}
+		}
+	}
+}
+
