bb67f763c469ca5c8462683f730d7adfd1bfa5e0
Harisaran <harisaran777s@gmail.com>
2025-09-23 06:10:53 +0530

    using String Buffer

---
+++ StringBuffer3.java
@@ -0,0 +1,7 @@
+public class StringBuffer3 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.replace(1,3,"Jaava");
+        System.out.println(sb);
+    }
+}
